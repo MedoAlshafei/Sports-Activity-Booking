8805dea6518f3399f2b44196263766e3355d2f41
MedoAlshafei <elshafey009@gmail.com>
2025-07-15 13:58:17 +0300

    Refactor home screen layout: Adjusted spacing in Row widget, updated OutlinedButton style, and commented out unused code for improved readability and maintainability.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import '../themes/app_colors.dart';
+// import '../themes/app_colors.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -50,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Row(
-                      spacing: 2,
+                      spacing: 8,
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: [
                         Text(
@@ -68,6 +68,9 @@ class _MyHomePageState extends State<MyHomePage> {
                             onPressed: () {},
                             icon: Icon(Icons.person),
                             color: Colors.white,
+                            style: IconButton.styleFrom(
+                              backgroundColor: Colors.black26,
+                            ),
                             tooltip: 'Profile',
                             iconSize: 24,
                           ),
@@ -136,6 +139,35 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ],
           ),
+          const SizedBox(height: 35),
+          Row(
+            spacing: 16,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                  maximumSize: const Size(70, 50),
+                  minimumSize: const Size(70, 50),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 8,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                child: Text(
+                  'str',
+                  style: GoogleFonts.eduSaBeginner(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: colorScheme.onSurface,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -159,144 +191,6 @@ class _MyHomePageState extends State<MyHomePage> {
         elevation: 24,
         child: Icon(Icons.add),
       ),
-
-      // Uncomment the following lines to add an AppBar and body content
-      // appBar: AppBar(
-      //   backgroundColor: colorScheme.primary,
-      //   title: Text(
-      //     'Welcome, Mohamed',
-      //     style: GoogleFonts.eduSaBeginner(
-      //       fontSize: 20,
-      //       fontWeight: FontWeight.w600,
-      //       color: colorScheme.onPrimary,
-      //     ),
-      //   ),
-      //   actions: [IconButton(icon: const Icon(Icons.person), onPressed: () {})],
-      //   elevation: 0,
-      // ),
-      // body: Column(
-      //   mainAxisAlignment: MainAxisAlignment.center,
-      //   children: <Widget>[
-      //     Container(
-      //       decoration: BoxDecoration(
-      //         borderRadius: BorderRadius.only(
-      //           bottomLeft: Radius.circular(16),
-      //           bottomRight: Radius.circular(16),
-      //         ),
-      //         gradient: LinearGradient(
-      //           colors: [
-      //             colorScheme.primary,
-      //             appColors.accent4,
-      //             appColors.accent1,
-      //           ], // Replace with your desired colors or use colorScheme if needed
-      //           begin: Alignment.topCenter,
-      //           end: Alignment.bottomCenter,
-      //         ),
-      //       ),
-      //       child: Column(
-      //         children: [
-      //           Row(
-      //             children: [
-      //               Text(
-      //                 'Test',
-      //                 textAlign: TextAlign.center,
-      //                 style: GoogleFonts.eduSaBeginner(
-      //                   fontSize: 24,
-      //                   fontWeight: FontWeight.bold,
-      //                   color: colorScheme.onSurface,
-      //                 ),
-      //               ),
-      //             ],
-      //           ),
-      //           SizedBox(height: 50),
-      //         ],
-      //       ),
-      //     ),
-      //     const SizedBox(height: 24),
-      //     Icon(Icons.sports_soccer, size: 100, color: colorScheme.primary),
-      //     const SizedBox(height: 24),
-      //     Text(
-      //       'مرحباً بك في تطبيق حجز الرياضة',
-      //       textAlign: TextAlign.center,
-      //       style: GoogleFonts.eduSaBeginner(
-      //         fontSize: 24,
-      //         fontWeight: FontWeight.bold,
-      //         color: colorScheme.onSurface,
-      //       ),
-      //     ),
-      //     const SizedBox(height: 16),
-      //     Text(
-      //       'تم تسجيل دخولك بنجاح!',
-      //       textAlign: TextAlign.center,
-      //       style: GoogleFonts.eduSaBeginner(
-      //         fontSize: 16,
-      //         color: colorScheme.onSurface.withOpacity(0.7),
-      //       ),
-      //     ),
-      //     const SizedBox(height: 48),
-      //     Row(
-      //       mainAxisAlignment: MainAxisAlignment.center,
-      //       children: [
-      //         ElevatedButton(
-      //           style: ElevatedButton.styleFrom(
-      //             backgroundColor: appColors.accent1,
-      //             foregroundColor: Colors.white,
-      //             padding: const EdgeInsets.symmetric(
-      //               horizontal: 24,
-      //               vertical: 12,
-      //             ),
-      //             shape: RoundedRectangleBorder(
-      //               borderRadius: BorderRadius.circular(12),
-      //             ),
-      //           ),
-      //           onPressed: () {},
-      //           child: Text(
-      //             'حجز ملعب',
-      //             style: GoogleFonts.eduSaBeginner(
-      //               fontSize: 16,
-      //               fontWeight: FontWeight.w600,
-      //             ),
-      //           ),
-      //         ),
-      //         const SizedBox(width: 12),
-      //         ElevatedButton(
-      //           style: ElevatedButton.styleFrom(
-      //             backgroundColor: appColors.success,
-      //             foregroundColor: Colors.white,
-      //             padding: const EdgeInsets.symmetric(
-      //               horizontal: 24,
-      //               vertical: 12,
-      //             ),
-      //             shape: RoundedRectangleBorder(
-      //               borderRadius: BorderRadius.circular(12),
-      //             ),
-      //           ),
-      //           onPressed: () {},
-      //           child: Text(
-      //             'حجوزاتي',
-      //             style: GoogleFonts.eduSaBeginner(
-      //               fontSize: 16,
-      //               fontWeight: FontWeight.w600,
-      //             ),
-      //           ),
-      //         ),
-      //       ],
-      //     ),
-      //   ],
-      // ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {
-      //     // Add new booking
-      //   },
-      //   shape: RoundedRectangleBorder(
-      //     borderRadius: BorderRadiusGeometry.circular(25),
-      //   ),
-      //   enableFeedback: true,
-      //   backgroundColor: colorScheme.primary,
-      //   foregroundColor: colorScheme.onPrimary,
-      //   child: const Icon(Icons.add),
-      // ),
     );
   }
 }
