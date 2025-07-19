9da584abbe188702dc0c87d056511e17fa5ff55f
MedoAlshafei <elshafey009@gmail.com>
2025-07-19 08:28:20 +0300

    Create Button widget to show Day task

---
+++ lib/widgets/custom_days_btn.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class DaysButton extends StatelessWidget {
+  const DaysButton({
+    super.key,
+    required this.colorScheme,
+    required this.dayName,
+  });
+
+  final ColorScheme colorScheme;
+  final String dayName;
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      onPressed: () {},
+      style: OutlinedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.r),
+        ),
+      ),
+      child: Text(
+        dayName,
+        style: GoogleFonts.eduSaBeginner(
+          fontSize: 12.sp,
+          fontWeight: FontWeight.w600,
+          color: colorScheme.onSurface,
+        ),
+      ),
+    );
+  }
+}
