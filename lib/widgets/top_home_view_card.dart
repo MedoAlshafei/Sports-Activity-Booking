37d9c2bdfc894e64ed8a656f641e829271194dbf
MedoAlshafei <elshafey009@gmail.com>
2025-07-19 08:44:17 +0300

    Create TopHomeViewCard widget for displaying gym card details

---
+++ lib/widgets/top_home_view_card.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sports_activity_booking/widgets/gym_card_details.dart';
+
+class TopHomeViewCard extends StatelessWidget {
+  const TopHomeViewCard({super.key, required this.colorScheme});
+
+  final ColorScheme colorScheme;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.only(top: 35, right: 16, left: 16, bottom: 48),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(18.r),
+              bottomRight: Radius.circular(18.r),
+            ),
+            color: colorScheme.primary,
+          ),
+        ),
+        Positioned(
+          top: 0,
+          left: 20,
+          right: 20,
+          child: GymCardDetails(colorScheme: colorScheme),
+        ),
+      ],
+    );
+  }
+}
