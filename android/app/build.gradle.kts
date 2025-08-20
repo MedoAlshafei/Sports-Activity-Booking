cdcfa5194a765466dbd8d68541a38462ae613c93
MedoAlshafei <elshafey009@gmail.com>
2025-08-21 02:51:44 +0300

    Update package namespace to "com.atras.app", adjust application ID, and add asset directory for images.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.sportsactivitybooking"
+    namespace = "com.atras.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "Atras"
+        applicationId = "com.atras.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/atras/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/atras/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.sportsactivitybooking
+package com.atras.app
 
 import io.flutter.embedding.android.FlutterActivity
 
